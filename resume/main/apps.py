99b5bdc0a5e21e90dea1af106b1c6b798043784e
devendra prasad <devendraprasad1984@gmail.com>
2021-10-30 18:12:49 +0530

    update

--- resume/main/apps.py
+++ resume/main/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class MainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'main'
+
+    # overriding signals to customer dispatcher
+    def ready(self):
+        # from resume.main import signals
+        import main.signals
